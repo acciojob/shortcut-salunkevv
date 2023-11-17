8cb23ea258937f40ee185bc057033cfb1a79f15a
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-17 11:32:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function shortcut(s1, s2) {
   // your code here
+	let ans=" ";
+	if(s1.charAt(0)=="" || s2.charAt(0)=="")
+	{
+		return ans;
+	}
+	else
+	{
+		ans= s1.charAt(0)+s2.charAt(0);
+	}
+	return ans;
 }
-
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
